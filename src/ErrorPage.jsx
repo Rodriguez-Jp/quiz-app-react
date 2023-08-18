896374e7bbe01153694644c7c0d1532835b2021f
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-08-17 22:17:49 -0500

    Added the error page

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -3,10 +3,16 @@ import { useRouteError, Link } from "react-router-dom";
 export default function ErrorPage() {
   const error = useRouteError();
   return (
-    <div>
-      <h1>Error</h1>
-      <p>Page wasnt found!</p>
-      <Link to={"/"}>Get back to main page</Link>
-    </div>
+    <>
+      <div className="w-full h-screen flex flex-col justify-center items-center pb-32">
+        <h1 className="text-red-500 font-bold text-4xl p-2">Error :(</h1>
+        <p className="text-2xl font-light">
+          Page wasn't found, please check the url
+        </p>
+        <Link to={"/"} className="bg-customBlue p-2 text-white rounded-lg mt-5">
+          Get back to main page
+        </Link>
+      </div>
+    </>
   );
 }
