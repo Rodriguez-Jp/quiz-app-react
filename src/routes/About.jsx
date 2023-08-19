11b5308a117b20dc2b37d726da39244209260884
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-08-18 22:36:52 -0500

    Added a smooth animation for the about section

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function About() {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 700);
+    setLoaded(false);
+  }, []);
+
   return (
     <>
-      <div className="absolute w-full h-full -z-10 skew-y-[-16deg] origin-top-left inset-0 bg-slate-900"></div>
+      <div
+        className={`absolute w-full h-full -z-10 origin-top-left inset-0 bg-slate-900 ${
+          loaded ? "skew-y-[-16deg]" : ""
+        } transition-transform duration-1000`}
+      ></div>
       <div className="max-w-3xl mx-auto text-white mt-20">
         <h1 className="font-extrabold text-4xl ">About the developer</h1>
         <h2 className="font-semibold text-xl mt-10">
